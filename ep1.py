74db70c5f943396bf6fd14bbeef0c679e21276db
moti <leu_k@hotmail.com>
2018-05-06 14:55:53 -0300

    estrutura ok

--- ep1.py
+++ ep1.py
@@ -1,7 +1,31 @@
 class Question():
-	a
-	b
 
 	def __init__(self, a ,b):
 		self.a = a
 		self.b = b
+
+
+def ler_questoes():
+
+
+
+def TRI(aluno, question):
+
+
+
+def exponencial(valor):
+
+
+
+def problema2_1():
+
+
+def problema2_2():
+
+
+def problema2_3():
+
+
+
+if __name__=='__main__':	
+main()
