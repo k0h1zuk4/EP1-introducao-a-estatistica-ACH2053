1410598ec8d5bc59150df8b36907b6225e5b1dfe
moti <leu_k@hotmail.com>
2018-05-07 14:13:15 -0300

    2-1 parcialmente feito

--- ep1.py
+++ ep1.py
@@ -14,7 +14,6 @@ aluno5 = 1.0
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-
 class Questao():
 	a = 0.0
 	b = 0.0
@@ -22,7 +21,6 @@ class Questao():
 	def __init__(self, a ,b):
 		self.a = a
 		self.b = b
-
 
 
 def sortear_questoes(questoes, numero):
@@ -42,17 +40,30 @@ def ler_questoes():
 			questoes.append(questao_atual)
 	return questoes
 
+
 def TRI(aluno, questao):
 	valor = math.exp(questao.a*(aluno - questao.b))
 	pr = valor / (1 + valor)
 	return pr
 
-def exponencial(valor):
-	return
 
+def problema2_1(questoes, numero, aluno):
+	soma_total = 0
 
-def problema2_1():
-	return
+	for number in range(10000):
+		questoes_sorteadas = sortear_questoes(questoes, numero)
+		
+		soma_das_pr_prova = 0
+
+		for questao in questoes_sorteadas:
+			soma_das_pr_prova = soma_das_pr_prova + TRI(aluno,questao)
+
+		soma_total = soma_total + (soma_das_pr_prova / 20)	
+
+	resultado = (soma_total / 10000)
+	return resultado	
+
+	
 
 def problema2_2():
 	return
@@ -63,5 +74,5 @@ def problema2_3():
 
 if __name__=='__main__':	
 	a = ler_questoes()
-	b = sortear_questoes(a, 2)
-	print(TRI(aluno5, b[1]))
+	b = sortear_questoes(a, 4)
+	print (problema2_1(a, 20, aluno5))
